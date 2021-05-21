cbdc3ba0ac73e250ee28f96ab55503dc6b5e2bc6
jean13011 <jean-13011@hotmail.fr>
2021-05-21 12:58:32 +0200

    controller with api connexion to french covid numbers made

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(CallApiService $callApiService): Response
     {
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'data' => $callApiService->getFranceData(),
         ]);
     }
 }
